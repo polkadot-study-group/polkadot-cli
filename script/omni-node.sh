88e8b91f382f295360ba98d73f17930975f14c7e
Arneil Paul Polican <policanarneilpaul@gmail.com>
2025-01-19 07:29:36 +0000

    removed script subdir

--- script/omni-node.sh
+++
@@ -1,2 +0,0 @@
-# ./nodes/polkadot-omni-node --chain ./chain-specs/polkadot-asset-hub.json -- --chain ./chain-specs/westend.json
-./binaries/polkadot-omni-node --chain ./chain-specs/chain_spec.json --dev
